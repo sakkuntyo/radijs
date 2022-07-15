c082a391a64abb1151d8c1451081d712a7cb105f
root <root@DESKTOP-GPH0HAQ.localdomain>
2022-07-15 21:29:13 +0900

    radijs

---
+++ index.js
@@ -0,0 +1,9 @@
+(function(module) {
+    'use strict';
+
+    module.exports.session = session;
+
+    function session(aaa) {
+        return "session ->" + aaa
+    };
+})(module);
---
+++ test.js
@@ -0,0 +1,3 @@
+const radijs = require("./index.js");
+
+console.log(radijs.session("aaa"));
