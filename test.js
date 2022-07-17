bacad24a28eee479aa428ca39d6250fda61eb85e
Daisuke Sasaki <20591351+sakkuntyo@users.noreply.github.com>
2022-07-17 16:15:29 +0900

    番組一覧取得API 追加

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const radijs = require("./index.js");
 
 (async() => {
+  console.log("トークン取得---")
   console.log(await radijs.get_authtoken());
+  console.log("局URL取得---")
   console.log(await radijs.get_bangumi_uri("TBS"));
+  console.log("局一覧取得")
+  console.dir(await radijs.get_station_id_list());
 })();
