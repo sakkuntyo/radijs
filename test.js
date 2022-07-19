b7188dea4e98159febb72fe59182910f787be555
Daisuke Sasaki <20591351+sakkuntyo@users.noreply.github.com>
2022-07-19 23:23:29 +0900

    フォルダ内に作成、削除する様に

--- test.js
+++ test.js
@@ -13,9 +13,14 @@ const child = require('child_process');
   console.log("局一覧取得")
   console.dir(await radijs.get_station_id_list());
   console.log("m3u8プレイリスト取得")
-  var bangumim3u8filename = Math.random().toString(32).substring(2) + "m3u8"
+  var dirName = "test"
+  try {
+    fs.mkdirSync(dirName);
+  } catch {
+  }
+  var bangumim3u8filepath = `${dirName}/` + Math.random().toString(32).substring(2) + ".m3u8"
   await new Promise((resolve, reject) => {
-    child.exec(`wget "${bangumiuri}" --header="X-Radiko-Authtoken: ${authtoken}" -O "${bangumim3u8filename}";sync`,
+    child.exec(`wget "${bangumiuri}" --header="X-Radiko-Authtoken: ${authtoken}" -O "${bangumim3u8filepath}";sync`,
       (error, stdout, stdin) => {
       if (error) {
         reject(error);
@@ -24,10 +29,12 @@ const child = require('child_process');
       }
     });
   });
-  var chunklisturi = (await radijs.get_m3u8_info(`./${bangumim3u8filename}`))[0];
-  var chunkm3u8filename = Math.random().toString(32).substring(2) + "m3u8"
+
+
+  var chunklisturi = (await radijs.get_m3u8_info(`./${bangumim3u8filepath}`))[0];
+  var chunkm3u8filepath = `${dirName}/` + Math.random().toString(32).substring(2) + ".m3u8"
   await new Promise((resolve, reject) => {
-    child.exec(`wget "${chunklisturi}" --header="X-Radiko-Authtoken: ${authtoken}" -O "${chunkm3u8filename}";sync`,
+    child.exec(`wget "${chunklisturi}" --header="X-Radiko-Authtoken: ${authtoken}" -O "${chunkm3u8filepath}";sync`,
       (error, stdout, stdin) => {
       if (error) {
         reject(error);
@@ -36,17 +43,19 @@ const child = require('child_process');
       }
     });
   });
-  chunklisturls = await radijs.get_m3u8_info(`./${chunkm3u8filename}`);
-  chunklisturls.forEach(async function(element, index, array){
-    await new Promise((resolve, reject) => {
-      child.exec(`wget "${element}" --header="X-Radiko-Authtoken: ${authtoken}";sync`,
-        (error, stdout, stdin) => {
-        if (error) {
+  chunklisturls = await radijs.get_m3u8_info(`${chunkm3u8filepath}`);
+  await new Promise((resolve, reject) => {
+    chunklisturls.forEach(async function(element, index, array){
+      child.exec(`cd ${dirName};wget "${element}" --header="X-Radiko-Authtoken: ${authtoken}";sync`, (error, stdout, stdin) => {
+      if (error) {
           reject(error);
         } else {
-          resolve(stdout); 
+	  if(chunklisturls.length == index + 1){
+            resolve(stdout); 
+	  }
         }
       });
     });
   });
+  fs.rmSync(`${dirName}`,{ recursive: true, force: true });
 })();
