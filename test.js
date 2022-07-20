5460ceab6cf5e7ad673da8444c9f7f1c0cdf0505
Daisuke Sasaki <20591351+sakkuntyo@users.noreply.github.com>
2022-07-20 23:01:11 +0900

    結合したmp3を取得する所まで作成

--- test.js
+++ test.js
@@ -37,62 +37,14 @@ const child = require('child_process');
   console.log("m3u8プレイリスト情報取得")
   console.log(await radijs.get_m3u8_info(chunkm3u8filePath))
 
-  /* 以下を実行するとm3u8取得->m3u8取得->aac結合したoutput.mp3 の作成までできる
-  var dirName = "test"
+  console.log("mp3 取得")
+  audioDirName = dirName + "/audio"
+  fs.rmSync(`${audioDirName}`,{ recursive: true, force: true });
   try {
     fs.mkdirSync(dirName);
-  } catch {
+  } catch(e) {
+    console.log(e)
   }
-  var bangumim3u8filepath = `${dirName}/` + Math.random().toString(32).substring(2) + ".m3u8"
-  await new Promise((resolve, reject) => {
-    child.exec(`wget "${bangumiuri}" --header="X-Radiko-Authtoken: ${authtoken}" -O "${bangumim3u8filepath}";sync`,
-      (error, stdout, stdin) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout); 
-      }
-    });
-  });
-
-
-  var chunklisturi = (await radijs.get_m3u8_info(`./${bangumim3u8filepath}`))[0];
-  var chunkm3u8filepath = `${dirName}/` + Math.random().toString(32).substring(2) + ".m3u8"
-  await new Promise((resolve, reject) => {
-    child.exec(`wget "${chunklisturi}" --header="X-Radiko-Authtoken: ${authtoken}" -O "${chunkm3u8filepath}";sync`,
-      (error, stdout, stdin) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout); 
-      }
-    });
-  });
-  chunklisturls = await radijs.get_m3u8_info(`${chunkm3u8filepath}`);
-  await new Promise((resolve, reject) => {
-    chunklisturls.forEach(async function(element, index, array){
-      child.exec(`cd ${dirName};wget "${element}" --header="X-Radiko-Authtoken: ${authtoken}";sync`, (error, stdout, stdin) => {
-      if (error) {
-          reject(error);
-        } else {
-	  if(chunklisturls.length == index + 1){
-            resolve(stdout); 
-	  }
-        }
-      });
-    });
-  });
-
-  // ffmpeg結合
-  await new Promise((resolve, reject) => {
-    child.exec(`cd "${dirName}";find ./*.aac | sed 's/^/file /g' | sort > concatfiles.txt;ffmpeg -f concat -safe 0 -i "concatfiles.txt" "./output.mp3"`, (error, stdout, stdin) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout);
-      }
-    });
-  });
-  //fs.rmSync(`${dirName}`,{ recursive: true, force: true });
-  */
+  chunklisturls = await radijs.get_m3u8_info(`${chunkm3u8filePath}`);
+  await radijs.download_mp3_file(chunkm3u8filePath, authtoken, audioDirName);
 })();
